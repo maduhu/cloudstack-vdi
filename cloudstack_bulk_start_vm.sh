3d1463c2a948c821ccd5cc614e8b07f029d1a97d
James McClune <falcon78921@users.noreply.github.com>
2018-08-02 15:15:04 -0400

    Create cloudstack_bulk_start_vm.sh

---
+++ cloudstack_bulk_start_vm.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Bulk Start VM's in CloudStack
+# falcon78921
+
+# First, we need a group ID
+
+read -p "What is the VM Group ID? " varGroupId
+read -p "What is the CloudStack UI URL? " varCloudStackUi
+
+# Now, we fetch VM's that are in the group and get their VM ID's
+
+curlUrl="http://${varCloudStackUi}:8096/client/api?command=listVirtualMachines&groupid=${varGroupId}&response=json"
+curl -s $curlUrl > vminstances.json
+
+# jq dis here file
+
+vmId=$(jq '.listvirtualmachinesresponse.virtualmachine[].id' vminstances.json | tr -d '"')
+
+# Array it up!
+
+vmIdArray=( $vmId )
+
+for i in "${vmIdArray[@]}"  
+do  
+    curlUrl="http://${varCloudStackUi}:8096/client/api?command=startVirtualMachine&id=${i}"
+    curl $curlUrl
+done  
+
+# Delete vminstances.json
+
+rm vminstances.json
